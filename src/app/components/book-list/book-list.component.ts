f1fdca7d4a377af9c247c9a8718343093f574a40
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-18 20:58:00 -0400

    Fixed heroku errors

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Book} from '../../models/book';
 import {Router} from '@angular/router';
 import {LoginService} from '../../services/login.service';
@@ -8,110 +8,112 @@ import {RemoveBookService} from '../../services/remove-book.service';
 import {MdDialog, MdDialogRef} from '@angular/material';
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  private selectedBook : Book;
-  private checked: boolean;
-  private bookList: Book[];
-  private allChecked: boolean;
-  private removeBookList: Book[] = new Array();
+    selectedBook: Book;
+    checked: boolean;
+    bookList: Book[];
+    allChecked: boolean;
+    removeBookList: Book[] = new Array();
 
-  constructor(
-    private getBookListService: GetBookListService,
-    private removeBookService: RemoveBookService,
-    private router:Router,
-    public dialog:MdDialog
-    ) { }
+    constructor(
+        private getBookListService: GetBookListService,
+        private removeBookService: RemoveBookService,
+        private router: Router,
+        public dialog: MdDialog
+    ) {
+    }
 
-  onSelect(book:Book) {
-    this.selectedBook=book;
-    this.router.navigate(['/viewBook', this.selectedBook.id]);
-  }
+    onSelect(book: Book) {
+        this.selectedBook = book;
+        this.router.navigate(['/viewBook', this.selectedBook.id]);
+    }
 
-  openDialog(book:Book) {
-    let dialogRef = this.dialog.open(DialogResultExampleDialog);
-    dialogRef.afterClosed().subscribe(
-      result => {
-        console.log(result);
-        if(result=="yes") {
-          this.removeBookService.sendBook(book.id).subscribe(
-            res => {
-              console.log(res);
-              this.getBookList();
-            }, 
-            err => {
-              console.log(err);
+    openDialog(book: Book) {
+        const dialogRef = this.dialog.open(DialogResultExampleDialog);
+        dialogRef.afterClosed().subscribe(
+            result => {
+                console.log(result);
+                if (result === 'yes') {
+                    this.removeBookService.sendBook(book.id).subscribe(
+                        res => {
+                            console.log(res);
+                            this.getBookList();
+                        },
+                        err => {
+                            console.log(err);
+                        }
+                    );
+                }
             }
-            );
-        }
-      }
-      );
-  }
-
-  updateRemoveBookList(checked:boolean, book:Book) {
-    if(checked) {
-      this.removeBookList.push(book);
-    } else {
-      this.removeBookList.splice(this.removeBookList.indexOf(book), 1);
+        );
     }
-  }
 
-  updateSelected(checked: boolean) {
-    if(checked) {
-      this.allChecked = true;
-      this.removeBookList=this.bookList.slice();
-    } else {
-      this.allChecked=false;
-      this.removeBookList=[];
-    }
-  }
-
-  removeSelectedBooks() {
-    let dialogRef = this.dialog.open(DialogResultExampleDialog);
-    dialogRef.afterClosed().subscribe(
-      result => {
-        console.log(result);
-        if(result=="yes") {
-          for (let book of this.removeBookList) {
-            this.removeBookService.sendBook(book.id).subscribe(
-              res => {
-
-              }, 
-              err => {
-              }
-              );
-          }
-          location.reload();
+    updateRemoveBookList(checked: boolean, book: Book) {
+        if (checked) {
+            this.removeBookList.push(book);
+        } else {
+            this.removeBookList.splice(this.removeBookList.indexOf(book), 1);
         }
-      }
-      ); 
-  }
+    }
 
-  getBookList() {
-    this.getBookListService.getBookList().subscribe(
-      res => {
-        console.log(res.json());
-        this.bookList=res.json();
-      }, 
-      error => {
-        console.log(error);
-      }
-      );
-  }
+    updateSelected(checked: boolean) {
+        if (checked) {
+            this.allChecked = true;
+            this.removeBookList = this.bookList.slice();
+        } else {
+            this.allChecked = false;
+            this.removeBookList = [];
+        }
+    }
 
-  ngOnInit() {
-    this.getBookList();
-  }
+    removeSelectedBooks() {
+        const dialogRef = this.dialog.open(DialogResultExampleDialog);
+        dialogRef.afterClosed().subscribe(
+            result => {
+                console.log(result);
+                if (result == 'yes') {
+                    for (const book of this.removeBookList) {
+                        this.removeBookService.sendBook(book.id).subscribe(
+                            res => {
+
+                            },
+                            err => {
+                            }
+                        );
+                    }
+                    location.reload();
+                }
+            }
+        );
+    }
+
+    getBookList() {
+        this.getBookListService.getBookList().subscribe(
+            res => {
+                console.log(res.json());
+                this.bookList = res.json();
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    ngOnInit() {
+        this.getBookList();
+    }
 
 }
 
 @Component({
-  selector: 'dialog-result-example-dialog',
-  templateUrl: './dialog-result-example-dialog.html'
+    selector: 'dialog-result-example-dialog',
+    templateUrl: './dialog-result-example-dialog.html'
 })
 export class DialogResultExampleDialog {
-  constructor(public dialogRef: MdDialogRef<DialogResultExampleDialog>) {}
+    constructor(public dialogRef: MdDialogRef<DialogResultExampleDialog>) {
+    }
 }
--- src/app/components/view-book/view-book.component.ts
+++ src/app/components/view-book/view-book.component.ts
@@ -1,42 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Params, ActivatedRoute, Router} from '@angular/router';
 import {GetBookService} from '../../services/get-book.service';
 import {Book} from '../../models/book';
 
 @Component({
-  selector: 'app-view-book',
-  templateUrl: './view-book.component.html',
-  styleUrls: ['./view-book.component.css']
+    selector: 'app-view-book',
+    templateUrl: './view-book.component.html',
+    styleUrls: ['./view-book.component.css']
 })
 export class ViewBookComponent implements OnInit {
 
-  private book:Book = new Book();
-  private bookId: number;
+    book: Book = new Book();
+    bookId: number;
 
-  constructor(private getBookService:GetBookService,
-    private route:ActivatedRoute, private router:Router) { }
+    constructor(private getBookService: GetBookService,
+                private route: ActivatedRoute, private router: Router) {
+    }
 
-  onSelect(book:Book) {
-    this.router.navigate(['/editBook', this.book.id])
-    // .then(s => location.reload())
-    ;
-  }
+    onSelect(book: Book) {
+        this.router.navigate(['/editBook', this.book.id])
+        // .then(s => location.reload())
+        ;
+    }
 
-  ngOnInit() {
-    this.route.params.forEach((params: Params) => {
-      this.bookId = Number.parseInt(params['id']);
-    });
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            this.bookId = Number.parseInt(params['id']);
+        });
 
-    this.getBookService.getBook(this.bookId).subscribe(
-      res => {
-        this.book = res.json();
-      },
-      error => {
-        console.log(error);
-      }
-    );
+        this.getBookService.getBook(this.bookId).subscribe(
+            res => {
+                this.book = res.json();
+            },
+            error => {
+                console.log(error);
+            }
+        );
 
-    
-  }
+
+    }
 
 }
